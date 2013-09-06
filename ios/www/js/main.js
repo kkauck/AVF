ef6f93f3ce920ae157a29d162a6edcf517e6b460
Kyle Kauck <kyle0nlin3@fullsail.edu>
2013-09-05 18:25:11 -0600

    Initial commit for AVF 1309
    Sept. 5

---
+++ ios/www/js/main.js
@@ -0,0 +1,46 @@
+/*
+Kyle Kauck
+AVF 1309
+Week One Demo App
+Sept. 4, 2013
+*/
+
+//Any code needed for my index page
+$("#index").on("pageinit", funciton(){
+	
+});
+
+//Code needed for Connection
+$(document).on("pageinit", "#connection", function(){
+	
+});
+
+//Code needed for Camera
+$(document).on("pageinit", "#camera", function(){
+	
+});
+
+//Code needed for Geolocation
+$(document).on("pageinit", "#geo", function(){
+	
+});
+
+//Code needed for Notifications
+$(document).on("pageinit", "#notification", function(){
+	
+});
+
+//Code needed for Contacts
+$(document).on("pageinit", "#contacts", function(){
+	
+});
+
+//Code needed for Instagram
+$(document).on("pageinit", "#instagram", function(){
+	
+});
+
+//Code needed for Weather
+$(document).on("pageinit", "#weather", function(){
+	
+});
